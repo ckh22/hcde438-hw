fa3553a8c9c281c11d2ba94cee1c47ec1fbfa382
ckh22 <45274762+ckh22@users.noreply.github.com>
2021-03-15 08:07:40 -0700

    update

--- final-proj/src/App/Components/Navbar/Nav.jsx
+++ final-proj/src/App/Components/Navbar/Nav.jsx
@@ -1,35 +1,6 @@
 import React from 'react'
-import {
-    AppBar,
-    Toolbar,
-    Typography,
-    Button,
-    IconButton
-} from '@material-ui/core'
-import {makeStyles} from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu'
-import { signInWithGoogle } from '../../Database/db';
-import {useAuthState} from 'react-firebase-hooks/auth'
-import {auth} from '../../Database/db'
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-        backgroundColor: '#cccccc',
-        color: 'black'
-    },
-    menuButton: {
-        marginRight: theme.spacing(2)
-    },
-    title: {
-        flexGrow: 1
-    }
-
-}));
 
 const Nav = () => {
-    const [user] = useAuthState(auth)
-    const classes = useStyles();
     return (
         <nav className='nav'></nav>
     )
